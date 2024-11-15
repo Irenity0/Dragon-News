138be59943b1db61f486ce5d457ad1548cb5f6d0
Irenity <lilithkard77@gmail.com>
2024-11-15 20:32:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h2>uwu</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <>
+        <h1>dis header</h1>
+        </>
+    );
+};
+
+export default Header;
