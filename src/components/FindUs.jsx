e19f69413cf1762a2b41381f2197f23f8c26a305
Irenity <lilithkard77@gmail.com>
2024-11-16 15:24:32 +0600

    find us section done

---
+++ src/components/FindUs.jsx
@@ -0,0 +1,14 @@
+const FindUs = () => {
+    return (
+        <div>
+            <h2 className="text-xl font-semibold mb-3">Find Us</h2>
+            <div className="join join-vertical flex *:bg-base-100 w-full">
+              <button className="btn join-item justify-start">Facebook</button>
+              <button className="btn join-item justify-start">Github</button>
+              <button className="btn join-item justify-start">X</button>
+            </div>
+        </div>
+    );
+};
+
+export default FindUs;
